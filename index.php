e34f26ca89c361aab7572350eac33280966e01e4
Carlo1891 <carlo.18@live.it>
2022-08-22 15:47:00 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!-- 
+Creare una variabile con un paragrafo di testo a vostra scelta.
+Stampare a schermo il paragrafo e la sua lunghezza.
+Una parola da censurare viene passata dall'utente tramite parametro GET.
+Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>BadWords</title>
+    </head>
+    
+    <body>
+        
+    </body>
+</html>
